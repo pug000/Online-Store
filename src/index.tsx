c6b0abbd7d3a88174934b1a4afb8836b0cc94dd2
Roman Troshin <roman.troshin98@gmail.com>
2022-07-18 04:05:28 +0300

    feat: add self-esteem

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { selfEsteem } from './selfEsteem';
 
 const root = document.getElementById('root');
 
 if (!root) throw new Error;
+
+selfEsteem();
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
