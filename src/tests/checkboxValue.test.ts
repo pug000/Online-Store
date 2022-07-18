1efb338e8222fc978877cb459f5f732225b8b2a6
Roman Troshin <roman.troshin98@gmail.com>
2022-07-18 20:03:37 +0300

    feat: add checkboxValue test

---
+++ src/tests/checkboxValue.test.ts
@@ -0,0 +1,19 @@
+import { checkboxFilter } from "../settings";
+
+describe('checkbox function', () => {
+  test('correct value', () => {
+    expect(checkboxFilter('asd', ['asd'])).toBeTruthy();
+    expect(checkboxFilter('vcbfd', ['asdt64', 'vcbfd'])).toBeTruthy();
+  });
+
+  test('incorrect value', () => {
+    expect(checkboxFilter('vcbfd', ['sdsdf', '435u89', 'yukjlg'])).toBeFalsy();
+    expect(checkboxFilter('myij', ['ghu5fg', '789uy', ''])).toBeFalsy();
+  });
+
+  test('empty value', () => {
+    expect(checkboxFilter('vcbfd', [])).toBeTruthy();
+    expect(checkboxFilter('', [])).toBeFalsy();
+    expect(checkboxFilter('', ['sfdc'])).toBeFalsy();
+  });
+});
