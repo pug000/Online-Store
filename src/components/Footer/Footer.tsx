8a91929093e9a59cc2f150183fd5664035236fae
Roman Troshin <roman.troshin98@gmail.com>
2023-02-13 00:10:10 +0300

    refactor: refactor footer classnames

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,22 @@
+import { memo } from 'react';
+
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerLine} />
-      <div className={styles.footerGithub}>
+    <footer className={styles.wrapper}>
+      <div className={styles.line} />
+      <div className={styles.github}>
         <a
-          className={styles.footerGithubLink}
+          className={`${styles.githubLink} ${styles.link}`}
           href="https://github.com/pug000"
           aria-hidden="true"
         />
       </div>
-      <div className={styles.footerYear}>2022</div>
-      <div className={styles.footerRsschool}>
+      <div className={styles.year}>2022</div>
+      <div className={styles.rsschool}>
         <a
-          className={styles.footerRsschoolLink}
+          className={`${styles.rsschoolLink} ${styles.link}`}
           href="https://rs.school/js/"
           aria-hidden="true"
         />
@@ -23,4 +25,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default memo(Footer);
